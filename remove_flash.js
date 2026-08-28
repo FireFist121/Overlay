ba0c088c108344b3e0674b3f9f0c5982bbce257d
FireFist121 <rawatshashwat191.com>
2026-08-28 14:00:51 +0530

    Remove flash emoji from Top Supporters

---
+++ remove_flash.js
@@ -0,0 +1,5 @@
+﻿const fs = require("fs");
+let content = fs.readFileSync("src/app/widget/page.tsx", "utf8");
+content = content.replace("<span>⚡ TOP SUPPORTERS</span>", "<span>TOP SUPPORTERS</span>");
+fs.writeFileSync("src/app/widget/page.tsx", content, "utf8");
+console.log("Removed flash emoji from widget!");
